bcf3461f4ca320b33284b9d5f7e87e0f900ed39d
kangeugine <kangeugine@gmail.com>
2015-08-30 14:52:01 -0700

    Feature vs Importance

--- Feature Selection.R
+++ Feature Selection.R
@@ -10,4 +10,40 @@
 # We still suffer from the curse of high dimensionality p >>> n
 # For us to use a wider selection of models, lets narrow down our features
 # We will use RandomForest to compute the importance of feature for classification
+
+# Prepare training scheme
 require(mlbench)
+control <- trainControl(method="repeatedcv", number=10, repeats=1)
+
+# For reproducable results
+set.seed(1)
+# RandomForest to determine Feature importance
+rfModel <- train(Y~., data=datFilter, 
+                   method="rf", 
+                   trControl=control,
+                   importance=TRUE,
+                   do.trace=FALSE
+)
+# Organize Feature Importance Data
+fullImportance <- varImp(rfModel, scale=FALSE)
+fullVarImp <- fullImportance$importance
+fullVarImp$varName <- row.names(fullVarImp)
+colnames(fullVarImp) <- c("Imp","Imp2","varName")
+fullVarImp <- fullVarImp[order(fullVarImp$Imp, decreasing=T),]
+
+# How many features are selected by Importance?
+hist(fullVarImp$Imp)
+lvl <- seq(1,2,0.05)
+noFeatvsImp <- data.frame(Imp=lvl, 
+                          noFeat=sapply(lvl, function(x) sum(fullVarImp$Imp>x))
+                          )
+require(ggplot2)
+ggplot(data=noFeatvsImp, aes(x=Imp,y=noFeat)) + 
+        geom_line(aes(color="blue", size=3)) +
+        ggtitle("No.Features by Importance Threshold") +
+        labs(x="Importance",y="Number of Features") + 
+        theme(plot.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=25, hjust=0)) +
+        theme(axis.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=22)) +
+        guides(colour=FALSE, size=FALSE)
+
+# We can select features by importance and compare models
