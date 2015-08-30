591b6e699f1739ef615e37023c90a599c867ef76
kangeugine <kangeugine@gmail.com>
2015-08-30 13:58:36 -0700

    Cleaned Up Codes for better readability

--- Data Cleaning.R
+++ Data Cleaning.R
@@ -1,45 +1,46 @@
-# Splunk Homework
+# Expression levels of genes involved in a genetically inherited disease of a population sample
+# 1.  Build a model to predict whether a new person will have the disease
+# 2.  Given a test dataset, predict the disease state of each individual (test.txt)
+# 3.  Evaluate the accuracy of your model 
+
+#####
+# Exploratory Analysis and Data Cleaning
+#####
+
+# Import Data
 setwd("C:/Users/Golden/Desktop/Splunk/gene_expression_data")
 genex <- read.table("./genex.txt", header=FALSE, sep=" ")
-train <- read.table("./training.txt",header=TRUE, sep=" ")
+data <- read.table("./training.txt",header=TRUE, sep=" ")
 label <- read.table("./labels.txt", header=FALSE, sep=" ")
-#wtf <- read.table("./test.txt",header=TRUE, sep=" ")
 
-# Convert label to 1 and 0
-train$y <- ifelse(label == 1, 1,0)
+# What is the dimension of data?
+dim(data)
 
-# Lets remove variables that do not have any variance
-# Use nearZerovariance in caret
+# High-dimensional data
+# Let's remove variables with no variance or near zero variance
 require(caret)
-var0 <- nearZeroVar(train, saveMetrics=TRUE)
-str(var0)
-head(var0[var0[,"zeroVar"] > 0, ],10) 
-head(var0[var0[,"zeroVar"] + var0[,"nzv"] > 0, ],20) 
-var0 <- nearZeroVar(train)
-train2 <- train[,-var0]
+var0 <- nearZeroVar(data)
+length(var0)
+# 3245 features will be removed from data
+dataZero <- data[,-var0]
 
-# What should we do about missing values?
-noNA <- sapply(train2, function(x) sum(is.na(x)))
-colNA <- which(noNA > 0)
-for(i in 1:length(colNA)){
-        print(which(is.na(train2[,colNA[i]])))
-}
-# NAs are spread out so lets try kNN imputation to fill in the values
-#install.packages("DMwR")
+# Are there any features with missing values?
+noNA <- sapply(dataZero, function(x) sum(is.na(x)))
+table(noNA)
+# 6 Columns have missing values
+# Not many but lets try to fill out the missing values with imputation
+
+# Fill in NA values using the k Nearest Neighbours
 require(DMwR)
-dataImpute <- knnImputation(train2, k=10)
+dataImpute <- knnImputation(dataZero, k=10)
 
-# Let's remove features with more than 0.70 of correlation
-#install.packages("corrplot")
+# Let's try to remove the effects of Multicollinearity
+# Remove features with more than 0.70 of correlation
 require(corrplot)
-data.scale <- scale(dataImpute, center=TRUE, scale=TRUE)
-# Compute correlation matrix
-corMat <- cor(data.scale)
-
-# Set correlation threshold at 0.70
-install.packages("caret")
+dataScale <- scale(dataImpute, center=TRUE, scale=TRUE)
+corMat <- cor(dataScale)
 require(caret)
 highlyCor <- findCorrelation(corMat, 0.70)
-data.filter <- as.data.frame(data.scale[,-highlyCor])
-data.filter$Y <- ifelse(label == 1, 1,0)
-data.filter$Y <- as.factor(data.filter$Y)
+dataFilter <- as.data.frame(dataScale[,-highlyCor])
+dataFilter$Y <- ifelse(label == 1, 1,0)
+dataFilter$Y <- as.factor(dataFilter$Y)
